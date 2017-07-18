eda5d915a54ee115e840f6b3333aff36c1db2df3
Eric Saupe <ericsaupe@gmail.com>
2017-07-18 12:51:13 -0600

    Use options page

--- accept-timezone.js
+++ accept-timezone.js
@@ -1,22 +1,18 @@
-var timezone;
+var timezone
 
-chrome.storage.local.get("accept-timezone", function(item){
-  if (Object.keys(item).length === 0 && item.constructor === Object) {
-    timezone = 'Etc/UTC'
-  } else {
-    timezone = item["accept-timezone"];
-  }
-  setListener();
-});
-
-function setListener() {
-  chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-      details.requestHeaders.push({ name: 'Accept-Timezone', value: timezone })
-      console.log(details.requestHeaders);
-      return { requestHeaders: details.requestHeaders };
-    },
-    {urls: ['<all_urls>']},
-    [ 'requestHeaders' ]
-  );
-}
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    chrome.storage.local.get('timezone', function(item) {
+      if (!Object.keys(item).length && item.constructor === Object) {
+        timezone = 'Etc/UTC'
+      } else {
+        timezone = item['timezone']
+      }
+    })
+    details.requestHeaders.push({ name: 'Accept-Timezone', value: timezone })
+    console.log(details.requestHeaders)
+    return { requestHeaders: details.requestHeaders }
+  },
+  {urls: ['<all_urls>']},
+  ['requestHeaders']
+);
---
+++ options.js
@@ -0,0 +1,28 @@
+// Saves options to chrome.storage
+function save_options() {
+  var timezone = document.getElementById('timezone').value;
+  chrome.storage.sync.set({
+    timezone: timezone
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'Etc/UTC'
+  chrome.storage.sync.get({
+    timezone: 'Etc/UTC'
+  }, function(items) {
+    document.getElementById('timezone').value = items.timezone;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
